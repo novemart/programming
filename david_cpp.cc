f7876bea2c814e73b86e5125afc85178e8c57065
= <=>
2021-01-29 10:40:00 +0000

    added a print function to david's cpp program

--- david_cpp.cc
+++ david_cpp.cc
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout<<"Hello Martina!";
-	cout<<"Changes made here."<<endl;
-	cout<<"I am trying out the pull request stuff"<<endl;
+void print_Function()
+{
+	cout << "This is a print function" << endl;
+}
+
+int main()
+{
+	cout << "Hello Martina!";
+	cout << "Changes made here." << endl;
+	cout << "I am trying out the pull request stuff" << endl;
+	print_Function();
 	return 0;
 }
